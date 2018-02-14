16e9bbe9ec9dbdca013523c82b5b2de404987c9d
kevinlee05 <kevinlee05@gmail.com>
2018-02-14 13:00:24 +0800

    user views hompage, postgres settings

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,5 @@
+class CurrenciesController < ApplicationController
+    def show
+    end
+end
+
---
+++ spec/features/user_views_homepage_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "User views homepage" do
+    scenario "successfully" do
+        visit root_path
+        expect(page).to have_headline_text
+    end
+
+    private
+        def have_headline_text
+            have_text ("Values of Cryptocurrencies")
+
+        end
+
+end
